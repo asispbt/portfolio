acc898d02d26fcb7be5730ce64685f3b3c476d77
asispbt <ashishjha_ug@ei.nits.ac.in>
2022-06-28 23:11:23 +0530

    deploy v1.3

---
+++ build/precache-manifest.2b2409c07572e97abf0e2c06a52c7bfe.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "691da55c6bd1b1e4f393",
+    "url": "/static/css/main.18cd6c18.chunk.css"
+  },
+  {
+    "revision": "691da55c6bd1b1e4f393",
+    "url": "/static/js/main.691da55c.chunk.js"
+  },
+  {
+    "revision": "269d177dfa7b1bf5e155",
+    "url": "/static/js/1.269d177d.chunk.js"
+  },
+  {
+    "revision": "6c4506b07c62d66443bc",
+    "url": "/static/css/2.a564d2b0.chunk.css"
+  },
+  {
+    "revision": "6c4506b07c62d66443bc",
+    "url": "/static/js/2.6c4506b0.chunk.js"
+  },
+  {
+    "revision": "7ac455435b7f93cbf8f6",
+    "url": "/static/js/runtime~main.7ac45543.js"
+  },
+  {
+    "revision": "8f5ce27564945d2c9a10ef827549a78c",
+    "url": "/static/media/facebook.8f5ce275.png"
+  },
+  {
+    "revision": "d492efc706db983e74258dbd348f2208",
+    "url": "/static/media/linkedin.d492efc7.png"
+  },
+  {
+    "revision": "5c570427ee23f69853d28aec805eee79",
+    "url": "/static/media/instagram.5c570427.png"
+  },
+  {
+    "revision": "ce081bc3e2c09a39ea71e6a9a5ee2fdd",
+    "url": "/static/media/me-profess.ce081bc3.png"
+  },
+  {
+    "revision": "ee7043c159dc3a7dafcdd6184377a438",
+    "url": "/static/media/me-straight.ee7043c1.png"
+  },
+  {
+    "revision": "292471a4d926de48fe3c6f393a27fec2",
+    "url": "/static/media/project-001.292471a4.png"
+  },
+  {
+    "revision": "282d29859c9044b8347f67bc84616c4d",
+    "url": "/static/media/profile3_right.282d2985.png"
+  },
+  {
+    "revision": "91a2883ce2c6f5b2ec0a30b1bb641639",
+    "url": "/static/media/coding.91a2883c.webp"
+  },
+  {
+    "revision": "b5f436fabf68ef76ad85134700c3c6fd",
+    "url": "/static/media/badminton.b5f436fa.webp"
+  },
+  {
+    "revision": "a084a072d9b7a5f31683a02403058fa6",
+    "url": "/static/media/cricket.a084a072.jpg"
+  },
+  {
+    "revision": "c0af1de929ccb8d18c8e13d000276fa3",
+    "url": "/static/media/cycling.c0af1de9.jpg"
+  },
+  {
+    "revision": "5a7db5bb94885e4493f14e51749121ec",
+    "url": "/static/media/music.5a7db5bb.jpg"
+  },
+  {
+    "revision": "cf477fb56dea32609bc123f2344c2bc9",
+    "url": "/index.html"
+  }
+];
